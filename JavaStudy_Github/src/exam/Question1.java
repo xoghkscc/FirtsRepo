2eb1f5627c5749a2c94dd23f261973e495859fc3
KTH <xoghkscc@naver.com>
2021-04-19 14:39:19 +0900

    브렌치A 두 번째 진행

--- JavaStudy_Github/src/exam/Question1.java
+++ JavaStudy_Github/src/exam/Question1.java
@@ -2,7 +2,7 @@ package exam;
 
 public class Question1 {
 	public static void main(String[] args) {
-		System.out.println("nsadoisa");
-		System.out.println("dbsaui");
+		System.out.println("Branch A 생성 기념으로 업데이트!");
+		System.out.println("Branch A 한번 더!!");
 	}
 }
