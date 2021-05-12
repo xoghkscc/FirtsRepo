4857ff803df1efd812b00bb355601173bbe16610
김태환 <@14.36.40.240>
2021-05-12 18:40:52 +0900

    21.05.12 집에서 커밋 & 푸쉬 숙제 끝

--- JavaStudy/src/quiz/E03_SaveGame.java
+++ JavaStudy/src/quiz/E03_SaveGame.java
@@ -5,9 +5,11 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 import myobj.MyScanner;
 
@@ -21,53 +23,163 @@ public class E03_SaveGame {
 	Random ran;
 	HashMap<String, Integer> gameValue;
 
-	String computerWin = "컴퓨터의 승리 횟수";
-	String userWin = "유저의 승리 횟수";
-	String draw = "비긴 횟수";
+	String[] victoryJudgment = {"컴퓨터의 승리 횟수", "유저의 승리 횟수", "비긴 횟수"};
+	String name;
 
 	public static void main(String[] args) {
-		String name;
 		E03_SaveGame sg = new E03_SaveGame();
-
-		System.out.printf("1. 새로운 게임 시작  2. 게임 불러오기  3. 종료하기>>");
-		int userNum = new Scanner(System.in).nextInt();
-		switch (userNum) {
-		case 1:
-			name = MyScanner.input("새로운 사용자의 이름을 입력 >>");
-			sg.newGame(name);
-			break;
-		case 2:
-			name = MyScanner.input("불러올 사용자의 이름을 입력 >>");
-			sg.loadGame(name);
-			break;
-		case 3:
-
-			break;
-
-		default:
-			System.out.println("다시 입력하세요.");
-			break;
-		}
+		sg.gameStart();
 
 	}
+	
+	public void gameStart() {
+		while(true) {
+			System.out.printf("1. 새로운 게임 시작  2. 게임 불러오기  3. 사용자 목록 확인하기 4. 종료하기>>");
+			int userNum = new Scanner(System.in).nextInt();
+			switch (userNum) {
+			case 1:
+				System.out.printf("새로운 사용자의 이름을 입력 >>");
+				name = new Scanner(System.in).next();
+				newGame(name);
+				break;
+			case 2:
+				System.out.printf("불러올 사용자의 이름을 입력 >>");
+				name = new Scanner(System.in).next();
+				loadGame(name);
+				break;
+			case 3:
+				userList();
+				break;
+			case 4:
+				System.out.println("게임 끝!");
+				System.exit(0);
+				break;
+	
+			default:
+				System.out.println("다시 입력하세요.");
+				break;
+			}
+		}
+	}
 
+	public void userList() {
+		String dirName = "./note/user/";
+		File dir = new File(dirName);
+		String[] files = dir.list();
+		
+		Pattern regex = Pattern.compile("[.]");
+		
+		for (int i = 0; i < files.length; i++) {
+			String[] name_separation = regex.split(files[i]);
+			files[i] = name_separation[0];
+		}
+		System.out.println("유저 목록 : "+Arrays.toString(files));
+		
+		
+	}
+	
 	public void newGame(String name) {
 		gameValue = new HashMap<>();
 
-		File gameFile = new File(String.format("./note/%s.txt", name));
+		File gameFile = new File(String.format("./note/user/%s.txt", name));
+		if(gameFile.exists()) {
+			System.out.println("이미 존재하는 계정입니다.");
+		} else {
+			for (int i = 0; i < victoryJudgment.length; i++) {
+				gameValue.put(victoryJudgment[i], 0);
+			}
+	
+			gameValue = gameLogic(gameValue, name);
+			saveDate(gameValue, gameFile);
+		}
+	}
 
-		gameValue.put(computerWin, 0);
-		gameValue.put(userWin, 0);
-		gameValue.put(draw, 0);
-
-		gameValue = gameLogic(gameValue);
+	public void loadGame(String name) {
+		gameValue = new HashMap<>();
 		
+		File gameFile = new File(String.format("./note/user/%s.txt", name));
+		
+		try {
+			FileInputStream fin = new FileInputStream(gameFile);
+			int _byte;
+			try {
+				String date = "";
+				int index = 0;
+				while((_byte = fin.read()) != -1) {
+					if(_byte != (char)'\n') {
+						date += (char)_byte;
+					}else {
+						gameValue.put(victoryJudgment[index],Integer.parseInt(date));
+						date = "";
+						index++;
+					}
+				}
+				System.out.printf("현재 컴퓨터[%d]승 : %s[%d]승, 무승부[%d]회\n", gameValue.get(victoryJudgment[0]), name, gameValue.get(victoryJudgment[1]),gameValue.get(victoryJudgment[2]));
+				gameValue = gameLogic(gameValue, name);
+				saveDate(gameValue, gameFile);
+				fin.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("그런 이름은 존재하지 않음");
+		}
+	}
+
+	public HashMap<String, Integer> gameLogic(HashMap<String, Integer> gameValue, String name) {
+		
+		Random ran = new Random();
+		
+		while(true) {
+			try {
+				int choose = MyScanner.inputInt("1. 게임 하기  2. 종료하기 >>");
+				switch (choose) {
+				case 1:
+					int comChoose = ran.nextInt(3)+1;
+					int userChoose = MyScanner.inputInt("1. 가위, 2. 바위, 3. 보 >>");
+					String[] rock_paper_scissors = {"가위", "바위", "보"};
+					
+					System.out.printf("컴퓨터는[%s], %s는 [%s]를 냈습니다.\n", rock_paper_scissors[comChoose-1], name, rock_paper_scissors[userChoose-1]);
+					
+					if(comChoose == userChoose) {
+						System.out.println("비겼습니다.");
+						draw(gameValue, name);
+					} else if(comChoose > userChoose) {
+						if(comChoose==3 && userChoose == 1) {
+							userWin(gameValue, name);
+						}else {
+							comWin(gameValue, name);
+						}
+					} else if(comChoose < userChoose) {
+						if(comChoose==1 && userChoose == 3) {
+							comWin(gameValue, name);
+						}else {
+							userWin(gameValue, name);
+						}
+					}
+					
+					break;
+				case 2:
+					System.out.println("로비로 돌아갑니다.");
+					return gameValue;
+				default:
+					System.err.println("1 혹은 2만 입력해 주세요");
+				}
+			} catch (Exception e) {
+				System.err.println("올바른 숫자를 입력해 주세요.");
+			}
+		}
+	}
+	
+	public void saveDate(HashMap<String, Integer> gameValue, File gameFile) {
 		try {
 			FileOutputStream fout = new FileOutputStream(gameFile);
 			try {
-				fout.write(Integer.toString(gameValue.get(computerWin)).getBytes());
-				fout.write(Integer.toString(gameValue.get(userWin)).getBytes());
-				fout.write(Integer.toString(gameValue.get(userWin)).getBytes());
+				for (int i = 0; i < victoryJudgment.length; i++) {
+					fout.write(Integer.toString(gameValue.get(victoryJudgment[i])).getBytes());
+					fout.write('\n');
+				}
+				fout.close();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -76,90 +188,35 @@ public class E03_SaveGame {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 	}
-
-	public File loadGame(String name) {
-		gameValue = new HashMap<>();
-		
-		File gameFile = new File(String.format("./note/%s", name));
-		
-		try {
-			FileInputStream fin = new FileInputStream(gameFile);
-		} catch (FileNotFoundException e) {
-			System.out.println("그런 이름은 존재하지 않음");
-			e.printStackTrace();
-		}
-		
-		gameValue.put(computerWin, 0);
-		gameValue.put(userWin, 0);
-		gameValue.put(draw, 0);
-		
-		return gameFile;
-	}
-
-	public HashMap<String, Integer> gameLogic(HashMap<String, Integer> gameValue) {
-		
-		Random ran = new Random();
-		
-		while(true) {
-			switch (MyScanner.inputInt("1. 게임 하기  2. 종료하기 >>")) {
-			case 1:
-				int comChoose = ran.nextInt(3)+1;
-				int userChoose = MyScanner.inputInt("1. 가위, 2. 바위, 3. 보 >>");
-				String[] rock_paper_scissors = {"가위", "바위", "보"};
-				
-				System.out.printf("컴퓨터는[%s], 유저는 [%s]를 냈습니다.\n", rock_paper_scissors[comChoose-1], rock_paper_scissors[userChoose-1]);
-				
-				if(comChoose == userChoose) {
-					System.out.println("비겼습니다.");
-					draw(gameValue);
-				} else if(comChoose > userChoose) {
-					if(comChoose==3 && userChoose == 1) {
-						userWin(gameValue);
-					}else {
-						comWin(gameValue);
-					}
-				} else if(comChoose < userChoose) {
-					if(comChoose==1 && userChoose == 3) {
-						comWin(gameValue);
-					}else {
-						userWin(gameValue);
-					}
-				}
-				
-				break;
-
-			default:
-				System.out.println("로비로 돌아갑니다.");
-				return gameValue;
-			}
-		}
-	}
-
-	public void comWin(HashMap<String, Integer> gameValue) {
-		int score = gameValue.get(computerWin);
+	
+	
+	
+	public void comWin(HashMap<String, Integer> gameValue, String name) {
+		int score = gameValue.get(victoryJudgment[0]);
 		score++;
-		gameValue.put(computerWin, score);
+		gameValue.put(victoryJudgment[0], score);
 		System.out.println("============================================");
-		System.out.printf("컴퓨터의 승리 !! 컴퓨터[%d] : 유저[%d], 무승부[%d]\n", gameValue.get(computerWin), gameValue.get(userWin),gameValue.get(draw));
-		System.out.println("============================================");
-	}
-
-	public void userWin(HashMap<String, Integer> gameValue) {
-		int score = gameValue.get(userWin);
-		score++;
-		gameValue.put(userWin, score);
-		System.out.println("============================================");
-		System.out.printf("유저의 승리 !! 컴퓨터[%d] : 유저[%d], 무승부[%d]\n", gameValue.get(computerWin), gameValue.get(userWin),gameValue.get(draw));
+		System.out.printf("컴퓨터의 승리 !! 컴퓨터[%d]승 : %s[%d]승, 무승부[%d]회\n", gameValue.get(victoryJudgment[0]), name, gameValue.get(victoryJudgment[1]),gameValue.get(victoryJudgment[2]));
 		System.out.println("============================================");
 	}
 
-	public void draw(HashMap<String, Integer> gameValue) {
-		int score = gameValue.get(draw);
+	public void userWin(HashMap<String, Integer> gameValue, String name) {
+		int score = gameValue.get(victoryJudgment[1]);
 		score++;
-		gameValue.put(draw, score);
+		gameValue.put(victoryJudgment[1], score);
 		System.out.println("============================================");
-		System.out.printf("비김 !! 컴퓨터[%d] : 유저[%d], 무승부[%d]\n", gameValue.get(computerWin), gameValue.get(userWin),gameValue.get(draw));
+		System.out.printf("유저의 승리 !! 컴퓨터[%d]승 : %s[%d]승, 무승부[%d]회\n", gameValue.get(victoryJudgment[0]), name,gameValue.get(victoryJudgment[1]),gameValue.get(victoryJudgment[2]));
+		System.out.println("============================================");
+	}
+
+	public void draw(HashMap<String, Integer> gameValue, String name) {
+		int score = gameValue.get(victoryJudgment[2]);
+		score++;
+		gameValue.put(victoryJudgment[2], score);
+		System.out.println("============================================");
+		System.out.printf("비김 !! 컴퓨터[%d]승 : %s[%d]승, 무승부[%d]회\n", gameValue.get(victoryJudgment[0]), name, gameValue.get(victoryJudgment[1]),gameValue.get(victoryJudgment[2]));
 		System.out.println("============================================");
 	}
 
