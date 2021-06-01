d3eb7d65685ea9c1961eaaafeaad771b18fe3d75
xoghkscc <82793713+xoghkscc@users.noreply.github.com>
2021-06-01 22:08:28 +0900

    21.06.01 집에서 푸쉬

--- JavaStudy/src/jdc/A01_03Practice.java
+++ JavaStudy/src/jdc/A01_03Practice.java
@@ -6,27 +6,44 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import myobj.FileBaseHikariCP;
 import myobj.MyScanner;
 import myobj.MySeverConnection;
 
 public class A01_03Practice {
-	MySeverConnection mc = new MySeverConnection();
-	Connection conn = mc.connection();
+
 	PreparedStatement psmt;
 	PreparedStatement psmt2;
+	Connection conn;
+	FileBaseHikariCP cp;
+	HikariDataSource ds;
 	public static void main(String[] args) {
 		A01_03Practice ap = new A01_03Practice();
 		ap.start();
 	}
 	
 	public void start() {
+		cp = new FileBaseHikariCP();
+		ds = cp.getHikariDataSource();
+		try {
+			conn = ds.getConnection();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		while(true) {
-			switch (MyScanner.inputInt("1. 계산, 2. 재고, 3. 그만>>")) {
+			switch (MyScanner.inputInt("1. 계산, 2. 개별 조회, 3. 매출 조회 4. 그만>>")) {
 			case 1:
 				calculation();
 				break;
 			case 2:
 				showcnt();
+				break;
+			case 3:
+				showsales();
 				break;
 	
 			default:
@@ -37,10 +54,37 @@ public class A01_03Practice {
 		}
 	}
 	
+	public void showsales() {
+		String sql = "SELECT sales_number, sum(price*object_count) FROM test0531 INNER JOIN test0531conn USING (object_id) GROUP BY sales_number ORDER BY sales_number";
+		try {
+			psmt = conn.prepareStatement(sql);
+			ResultSet rs = psmt.executeQuery();
+			
+			while(rs.next()) {
+				System.out.printf("판매 번호 : %d, 매출 : %d\n",rs.getInt(1), rs.getInt(2));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
 	public void showcnt() {
+		FileBaseHikariCP cp = new FileBaseHikariCP();
+		HikariDataSource ds = cp.getHikariDataSource();
+		
 		int object_id = MyScanner.inputInt("판매번호(SALES_NEMBER)>>");
-		psmt = mc.preparedStatement("SELECT * FROM test0531 INNER JOIN test0531conn USING (object_id) WHERE sales_number = "+ object_id);
-		psmt2 = mc.preparedStatement("SELECT sum(price*object_count) FROM test0531 INNER JOIN test0531conn USING (object_id) WHERE sales_number = "+ object_id);
+		try {
+			psmt = conn.prepareStatement("SELECT * FROM test0531 INNER JOIN test0531conn USING (object_id) WHERE sales_number = ?");
+			psmt2 = conn.prepareStatement("SELECT sum(price*object_count) FROM test0531 INNER JOIN test0531conn USING (object_id) WHERE sales_number = ?");
+			psmt.setInt(1, object_id);
+			psmt2.setInt(1, object_id);
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
 		int sumPrice = 0;
 		try {
 			ResultSet rs = psmt.executeQuery();
@@ -56,12 +100,16 @@ public class A01_03Practice {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 	}
 	
 	public int max_sales_number() {
 		int sales_number=0;
-		psmt2 = mc.preparedStatement("SELECT max(sales_number) FROM test0531");
+		try {
+			psmt2 = conn.prepareStatement("SELECT max(sales_number) FROM test0531");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		ResultSet rs2;
 		try {
 			rs2 = psmt2.executeQuery();
@@ -84,7 +132,6 @@ public class A01_03Practice {
 			switch (MyScanner.inputInt("1. 새우깡, 2. 파, 3. 고구마, 4. 두부, 5. 삼겹살, 6. 우유, 7. 쌀, 8. 그만>>")) {
 			case 1:
 				shoppingNum = 1;
-				
 				break;
 			case 2:
 				shoppingNum = 2;
@@ -116,7 +163,12 @@ public class A01_03Practice {
 		}
 	}
 	public void shooping(int shoppingNum, int sales_number) {
-		psmt = mc.preparedStatement("SELECT * FROM test0531conn");
+		try {
+			psmt = conn.prepareStatement("SELECT * FROM test0531conn");
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		
 		int cnt = MyScanner.inputInt("몇 개 사쉴?");
 		String query;
